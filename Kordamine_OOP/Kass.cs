30664dc001ec8618de7025cf5627ce9725fafb62
opilane <opilane@HP-CZC6297B8L>
2022-09-09 08:59:00 +0300

    Add project files.

---
+++ Kordamine_OOP/Kass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_OOP
+{
+    class Kass : Koduloom
+    {
+        public enum toug { Sfinks, Burma, Ragdoll };
+        public toug Toug;
+
+
+        public Kass(toug Toug, string nimi, string varv, sugu loomaSugu, double kaal, int vanus, bool elav) : base(nimi, varv, loomaSugu, kaal, vanus, elav)
+        {
+            this.Toug = Toug;
+        }
+        public Kass(Kass kass)
+        {
+            this.Toug = kass.Toug;
+            this.nimi = kass.nimi;
+            this.varv = kass.varv;
+            this.loomaSugu = kass.loomaSugu;
+            this.kaal = kass.kaal;
+            this.vanus = kass.vanus;
+            this.elav = kass.elav;
+
+        }
+        public override void print_Haal()
+        {
+            Console.WriteLine("Myauu, Myauuu");
+        }
+
+        public override void print_Info()
+        {
+            Console.WriteLine($"Toug:{Toug} hamsters.\nNimi: {nimi}\nVärv on {varv}\nTa on {loomaSugu} ja tema kaal on {kaal} gramm\n{vanus} aastat vana {elav}\n");
+        }
+
+        public void muudatNimi(string uusNimi) { nimi = uusNimi; }
+        public void muudatVarv(string uusVarv) { varv = uusVarv; }
+        public void muudatKaal(int uusKaal) { kaal = uusKaal; }
+        public void muudatVanus(int uusVanus) { vanus = uusVanus; }
+    }
+}
---
+++ Kordamine_OOP/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kordamine_OOP;
+Console.WriteLine("-----Koer----\n");
+Koer koer = new Koer(Koer.toug.Taksa, "Sarik", "hall", Koduloom.sugu.isane, 35.5, 7, true);
+Koer bobik = new Koer(Koer.toug.Mastif, "Praim", "Must", Koduloom.sugu.isane, 50.7, 2, true);
+koer.print_Info();
+koer.print_Haal();
+//public void a = koer.print_Info();
+
+/*Koer veelkoer = new Koer(koer);
+veelkoer.muudaNimi("Barsik");
+veelkoer.muudaVarv("Hall");
+veelkoer.muudaKaal(40);
+veelkoer.muudaVanus(5);
+
+veelkoer.print_Info();*/
+
+Console.WriteLine("\n-----Kass----\n");
+
+Kass kass = new Kass(Kass.toug.Sfinks, "Murka", "Must", Koduloom.sugu.isane, 25, 1, true);
+kass.print_Info();
+kass.print_Haal();
+
+/*Hamsters veelhamsters = new Hamsters(hamsters);
+veelhamsters.muudatNimi("Jar");
+veelhamsters.muudatVarv("Valge");
+veelhamsters.muudatKaal(36);
+veelhamsters.muudatVanus(2);
+
+veelhamsters.print_Info();*/
+
+
+List<Koduloom> Kooduloomut = new List<Koduloom>();
+Kooduloomut.Add(koer);
+Kooduloomut.Add(bobik);
+Kooduloomut.Add(kass);
+Console.WriteLine("\n");
+
+StreamWriter text = new StreamWriter(@"..\..\..\Info_Koduloom.txt", false);
+foreach (Koduloom x in Kooduloomut)
+{
+    text.WriteLine(x.nimi + "," + x.varv + "," + x.vanus);
+}
+text.Close();
+
+List<Koer> koera = new List<Koer>();
+StreamReader lugeda = new StreamReader(@"..\..\..\Info_Koduloom.txt");
+string texta;
+while ((texta = lugeda.ReadLine()) != null)
+{
+    string[] rida = texta.Split(',');
+    koera.Add(new Koer(rida[0], rida[1]));
+}
+lugeda.Close();
+
+foreach (var item in koera)
+{
+    Console.WriteLine(item.nimi + " " + item.varv);
+}
+Console.ReadLine();
+
+/*string file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Koduloom_info.txt");
+using (StreamWriter text = new StreamWriter(file))
+{
+    text.WriteLine("-----Koer----\n");
+    text.WriteLine($"Toug:{koer.Toug}\nVärv:{koer.varv} \nNimi: {koer.nimi}\nTa on {koer.loomaSugu} ja tema kaal on {koer.kaal} kg\n{koer.vanus} aastat vana\n");
+    
+    text.WriteLine("\n-----Hamsters----\n");
+    text.WriteLine("{0} hamsters. Nimi: {1} {2} ta on {3} ja tema kaal on {4} gramm ja ta on {5} aastat vana", hamsters.Toug, hamsters.nimi, hamsters.varv, hamsters.loomaSugu, hamsters.kaal, hamsters.vanus);
+}*/
